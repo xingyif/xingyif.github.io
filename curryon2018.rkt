53e61ec63f71c5d3095a4ee2028b1f33cb9d176f
yifanxing <xing.yif@Husky.neu.edu>
2018-08-13 20:07:37 -0400

    pictures

---
+++ curryon2018.rkt
@@ -0,0 +1,57 @@
+#lang scribble/manual
+@(require scriblib/figure)
+@(require pict)
+
+@; TODO include pictures of me, amsterdam, and map
+@; TODO include slides in webpage
+
+@title[#:tag "curryon2018" #:style 'unnumbered]{@hyperlink["http://www.curry-on.org/2018/sessions/distributed-systems-protocols-and-their-vulnerabilities.html"]{Curry On, Amsterdam}}
+
+@centered{-------@bold{Distributed Systems Protocols and their Vulnerabilities}-------}
+
+@centered{Conference: @hyperlink["http://www.curry-on.org/2018/"]{Curry On Conf}}
+@centered{
+ Colocated conferences include @hyperlink["https://2018.ecoop.org/"]{ECOOP} and @hyperlink["https://conf.researchr.org/home/issta-2018"]{ISSTA}.
+ Venue:
+ @hyperlink["https://www.movenpick.com/en/"]{Movenpick}
+ Piet Heinkade 11,
+ 1019 BR Amsterdam, Netherlands
+}
+
+@hyperlink["http://www.curry-on.org/2018/"]{CurryOn Conf} is a conference focused on the intersection of emerging languages and emerging challenges in industry (e.g. big data or security), as well as new ideas and paradigms in software development. 
+
+Curry On also seeks to act as a conduit for ferrying understanding and ideas back and forth between industry and academic programming languages, software engineering, and systems research communities (amongst others).
+
+Curry On is a rare event where academic minds responsible for concepts and tools now invaluable to everyday software development – like functional programming, or generics in Java – collide with the movers and shakers in industry that are building next-generation systems and developing software engineering practices central to our entire industry. 
+
+Curry On will be held in a different European city year-to-year, and it will always be co-located with one of the top academic conferences in programming languages. This year, it's co-located with @hyperlink["https://2018.ecoop.org/"]{ECOOP}.
+
+
+@centered{
+ @image["./pics/curryon.png"]
+
+}
+
+@hyperlink["https://twitter.com/curry_on_conf"]{ReactSphere Twitter}
+
+
+@centered{------------------------------------------@bold{Talk Abstract:}---------------------------------------}
+
+Many messaging systems that are widely used in the industry, e.g., Kafka, use centralized distributed systems services to achieve reliability and consensus between servers. Companies in the industry use the services; however, only a few of them understand the details of the protocols. This talk brings the principles used in academia to the industry by introducing the common distributed systems protocols implemented underneath the popular services. In addition, this talk will compare the differences between how the protocols are used in both academia and the industry. It provides details of how the protocols, specifically Paxos and Raft, work, including how they elect leaders among servers, how they achieve consensus between machines, and how they reliability process and execute client commands. Therefore, it shows how the systems and services, which use the protocols, are enabled to have fault-tolerance, and to achieve confidentiality, integrity, authenticity, availability, etc.
+
+From the reliability and security point of view, the talk discusses how the protocols deal with machine failures, including leader failures and replicas failures. It shows the vulnerabilities and potential security issues exist in the protocols. Last but not least, we’ll take a look at what we can do to avoid the vulnerabilities when applying the academic theories in the industry.
+
+@centered{------------------------------------------@bold{Slides:}----------------------------------------------}
+
+
+@italic{Slides can be found @hyperlink["https://www.slideshare.net/YifanXing/consensus-algorithms"]{here}!}
+
+
+@centered{------------------------------------------@bold{Video:}----------------------------------------------}
+
+
+@italic{Video can be found @hyperlink["https://www.youtube.com/watch?v=Gi1yrW8_EPk&t=0s&list=PLLYi1o1pORgNRq6l5Ude2tb1HaIzvnkdB&index=11"]{here}!}
+
+
+
+
